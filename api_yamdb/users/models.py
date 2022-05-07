e34e9ac951549af84cd94204a8b433be539a3d09
Pavel <web2cap@gmail.com>
2022-05-07 20:48:20 +0200

    add users model, app, admin

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("user", "Пользователь"),
+        ("moderator", "Модератор"),
+        ("admin", "Администратор"),
+    )
+    email = models.EmailField(
+        "Электронная почта",
+        blank=False,
+        null=False,
+        max_length=254,
+    )
+    role = models.CharField(
+        "Роль",
+        choices=ROLE_CHOICES,
+        default="user",
+        blank=False,
+        max_length=32,
+    )
+    bio = models.TextField(
+        "Биография",
+        blank=True,
+        null=True,
+        default=None,
+    )
+
+    def __str__(self):
+        return self.username
