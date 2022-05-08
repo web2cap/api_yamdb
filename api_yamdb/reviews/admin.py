a2a8928e0654a0eff6c1032f5d71e42b0b77362f
kelkrsru <kelkrsru@gmail.com>
2022-05-08 13:23:37 +0700

    Add models in Reviews app, Add reviews in settings.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Category, Titles, Review, Comments
+
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Titles)
+admin.site.register(Review)
+admin.site.register(Comments)
