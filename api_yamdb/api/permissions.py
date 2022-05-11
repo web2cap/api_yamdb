a4c138ec9b6e1efb5a659ba756e0597d8ea64379
Pavel <web2cap@gmail.com>
2022-05-11 13:42:13 +0200

    graceful conditions in permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,19 +12,17 @@ class IsRoleAdmin(permissions.BasePermission):
     """Разрешает доступ пользователям с ролью admin и суперпользователям."""
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.is_admin or request.user.is_superuser
+        return request.user.is_authenticated and (
+            request.user.is_admin or request.user.is_superuser
+        )
 
 
 class MeOrAdmin(permissions.BasePermission):
     """Разрешает запрос на /me/. Полный доступ админам."""
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
         path_end = request.path_info.split("/")[-2]
-        return (
+        return request.user.is_authenticated and (
             request.user.is_admin
             or request.user.is_superuser
             or path_end == "me"
