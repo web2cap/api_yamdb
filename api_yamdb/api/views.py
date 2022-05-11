a984cd9bfb5ff4203725deff3132ebe51ad03322
Pavel <web2cap@gmail.com>
2022-05-11 15:27:11 +0200

    prepare

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -14,12 +14,44 @@ from .serializers import UserSerializer
 
 class AuthViewSet(viewsets.ModelViewSet):
     """Получение токена авторизации JWT в ответ на POST запрос, на адрес /token.
-    POST на корневой эндпоитн и другие типы запросов запрешены пермищенном."""
+    Регистрация пользователей на эндпоинте /signup.
+    POST на корневой эндпоитн и другие типы запросов запрешены пермищенном.
+    """
 
     permission_classes = (PostOnlyNoCreate,)
 
     @action(detail=False, methods=["post"])
     def token(self, request):
+        """Получение токена по username и confirmation_code."""
+
+        if "username" not in request.data:
+            return Response(
+                {"detail": "No username in request"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if "confirmation_code" not in request.data:
+            return Response(
+                {"detail": "No confirmation_code in request"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user = get_object_or_404(
+            User,
+            username=request.data["username"],
+        )
+        if user.confirmation_code != request.data["confirmation_code"]:
+            return Response(
+                {"detail": "Wrong confirmation_code"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        access_token = str(AccessToken.for_user(user))
+        return Response({"access": access_token})
+
+    @action(detail=False, methods=["post"])
+    def signup(self, request):
+        """Самостоятельная регистрация нового пользователя.
+        Создает пользователя по запросу.
+        Отправляет код подверждения пользователю на email."""
+
         if "username" not in request.data:
             return Response(
                 {"detail": "No username in request"},
