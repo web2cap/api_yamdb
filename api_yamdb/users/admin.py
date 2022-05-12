099ff1f800d2f9f61356ff51b1ec050c9f36b2a7
Pavel <web2cap@gmail.com>
2022-05-12 16:09:28 +0200

    fix add def admin.py

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-
-class CustomUserAdmin(UserAdmin):
-    list_display = (
-        "username",
-        "email",
-        "first_name",
-        "last_name",
-        "is_staff",
-        "role",
-    )
-    list_filter = ("role", "is_staff", "is_superuser", "is_active", "groups")
-
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
