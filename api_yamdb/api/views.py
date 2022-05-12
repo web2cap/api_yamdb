55c1308dd15cd4bfd41fd226019d27a8b9561e3b
Pavel <web2cap@gmail.com>
2022-05-12 03:35:09 +0200

    fix import

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+
 from users.models import User
 
 from .permissions import (
